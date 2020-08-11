531959f75e4b60dcacda4196820ea4eace999a14
Jacob Kumar S3786688 <s3786688@student.rmit.edu.au>
2020-08-11 21:12:18 +1000

    Initialised React

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Online Appointment Booking System</h1>
+    </div>
+  );
+}
+
+export default App;
