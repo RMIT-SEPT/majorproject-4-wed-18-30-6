62e3db36df22e7cf5aa9990c23a186bcafea4433
Jacob Kumar S3786688 <S3786688@student.rmit.edu.au>
2020-10-14 18:43:16 +1100

    Updated backend default URL for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import axios from "axios";
+
+axios.defaults.baseURL = "https://wed1830-backend.herokuapp.com";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
